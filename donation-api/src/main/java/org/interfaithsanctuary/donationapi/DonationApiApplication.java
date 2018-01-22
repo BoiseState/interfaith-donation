6f410463cff5a44d2f5ffdaaee0fbaf1cb075e90
Brendan Tierney <brendantierney@u.boisestate.edu>
2018-01-21 17:45:02 -0700

    created the Java Spring Boot starter application

---
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/DonationApiApplication.java
@@ -0,0 +1,12 @@
+package org.interfaithsanctuary.donationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationApiApplication.class, args);
+	}
+}
