22c2be449b28f698068e94079bd431ef04cc8c2a
Amy Feiling <amyfeiling@u.boisestate.edu>
2018-01-28 15:50:52 -0700

    updated imports

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/DonationApiApplication.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/DonationApiApplication.java
@@ -2,11 +2,6 @@ package org.interfaithsanctuary.donationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class DonationApiApplication {
