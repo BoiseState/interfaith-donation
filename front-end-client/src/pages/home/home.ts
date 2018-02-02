84944af66acf7e7a819e11a457fdd60c7d88e494
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-02-01 22:56:31 -0700

    Changed title and added account page button on navbar

--- front-end-client/src/pages/home/home.ts
+++ front-end-client/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AccountPage } from '../account/account';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  pushAccountPage() {
+    this.navCtrl.push(AccountPage);
   }
 
 }
