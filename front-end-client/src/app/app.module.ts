f6d0bda541f171f162e520666ba7b91e041db000
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-02-01 23:18:38 -0700

    Account page opens on navbar button click

--- front-end-client/src/app/app.module.ts
+++ front-end-client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AccountPage } from '../pages/account/account';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AccountPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AccountPage
   ],
   providers: [
     StatusBar,
