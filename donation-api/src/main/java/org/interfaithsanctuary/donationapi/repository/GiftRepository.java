e800025d60a2a3e9bf5a503fed325ba277c8424c
Amy Feiling <amyfeiling@u.boisestate.edu>
2018-02-04 18:21:12 -0700

    added gift controller

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/GiftRepository.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/GiftRepository.java
@@ -1,12 +1,11 @@
 package org.interfaithsanctuary.donationapi.repository;
 
-import org.interfaithsanctuary.donationapi.model.Alert;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "alerts", path = "alerts")
-public interface AlertRepository extends CrudRepository<Alert, Long> {
+@RepositoryRestResource(collectionResourceRel = "gifts", path = "gifts")
+public interface GiftRepository extends CrudRepository<Gift, Long> {
 
 }
