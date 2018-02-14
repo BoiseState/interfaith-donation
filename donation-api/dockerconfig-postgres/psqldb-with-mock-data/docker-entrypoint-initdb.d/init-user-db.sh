c0745b7dfe4f094bcac3b97f148679202b702581
Chandra Adhikari <adhikarichandra@icloud.com>
2018-02-13 17:57:25 -0700

    Finished all configuration

--- donation-api/dockerconfig-postgres/psqldb-with-mock-data/docker-entrypoint-initdb.d/init-user-db.sh
+++ donation-api/dockerconfig-postgres/psqldb-with-mock-data/docker-entrypoint-initdb.d/init-user-db.sh
@@ -3,12 +3,12 @@
 # stops the execution if a command|pipeline has an error; opposite behavior of shell
 set -e
 
-psql <<-EOSQL
+psql $POSTGRES_DB $POSTGRES_USER <<-EOSQL
+    ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
     \du
     \list
     GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$POSTGRES_USER";
     \connect charity
-    \dt
     \include /docker-entrypoint-initdb.d/mock-data/interfaith_DDL.sql
     \include /docker-entrypoint-initdb.d/mock-data/insert-data-sql/Users.sql
     \include /docker-entrypoint-initdb.d/mock-data/insert-data-sql/Callouts.sql
@@ -16,4 +16,5 @@ psql <<-EOSQL
     \include /docker-entrypoint-initdb.d/mock-data/insert-data-sql/Needs.sql
     \include /docker-entrypoint-initdb.d/mock-data/insert-data-sql/Alerts.sql
     \include /docker-entrypoint-initdb.d/mock-data/insert-data-sql/Gifts.sql
+    \dt
 EOSQL
