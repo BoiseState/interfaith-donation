a6eda17dc05d02fb990719533ef10516c737d5f4
TylerNicholls <tylermnicholls@gmail.com>
2018-02-15 12:33:54 -0700

    made sure these restful api endpoints were working

--- front-end-admin/src/services/alert-service.js
+++ front-end-admin/src/services/alert-service.js
@@ -1,6 +1,10 @@
-import {getJSON} from './helpers';
+import {getJSON, postJSON, patchJSON} from './helpers';
 
-const BASE_API_ROUTE = '/alerts';
+const BASE_API_ROUTE = 'alerts';
+
+export const createAlert = async alert => {
+  return postJSON(BASE_API_ROUTE, alert);
+};
 
 export const getAllAlerts = () => {
   getJSON(BASE_API_ROUTE).then(alerts => {
@@ -12,3 +16,8 @@ export const getAlertById = async id => {
   let alert = await getJSON(`${BASE_API_ROUTE}/${id}`);
   return alert;
 };
+
+export const updateAlert = async alert => {
+  return patchJSON(`${BASE_API_ROUTE}/${alert.id}`, alert);
+};
+
