b1a75dc07d4c5e91f351ffc2e96e31b5ac3fb739
Amy Feiling <amyfeiling@u.boisestate.edu>
2018-02-15 14:13:59 -0700

    added all gift controller crud operations

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/model/Gift.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/model/Gift.java
@@ -6,28 +6,43 @@ import javax.persistence.*;
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "gifts")
 public class Gift {
-	@Id
-	@Column(name = "gift_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-	private int giftId;
-	
-	@Column(name = "gift_quantity")
+    @Id
+    //@Column(name = "gift_id")
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name="gift_id", sequenceName="gift_id")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="gift_id")
+    private long giftId;
+
+    @Column(name = "gift_quantity")
     private int quantity;
-	
-	@Column(name = "create_date")
+
+    @Column(name = "gift_date")
     private Date giftDate;
-	
-	@Column(name = "donor_id")
+
+    @Column(name = "donor_id")
     private int donorId;
-	
-	@Column(name = "need_id")
+
+    @Column(name = "need_id")
     private int needId;
 
-    public int getGiftId() {
+    public Gift() {
+        super();
+    }
+
+    public Gift(int giftId, int quantity, Date giftDate, int donorId, int needId) {
+        super();
+        this.giftId = giftId;
+        this.quantity = quantity;
+        this.giftDate = giftDate;
+        this.donorId = donorId;
+        this.needId = needId;
+    }
+
+    public long getGiftId() {
         return giftId;
     }
 
-    public void setGiftId(int giftId) {
+    public void setGiftId(long giftId) {
         this.giftId = giftId;
     }
 
