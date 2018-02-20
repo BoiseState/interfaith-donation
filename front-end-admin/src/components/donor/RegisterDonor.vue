cff7fef5827121a25d4ffe478a07346127df3a9e
wils0651 <wils0651@hotmail.com>
2018-02-20 12:53:52 -0700

    Added some client side validation

--- front-end-admin/src/components/donor/RegisterDonor.vue
+++ front-end-admin/src/components/donor/RegisterDonor.vue
@@ -3,7 +3,11 @@
     <div class="jumbotron">
       <div class="container">
         <!-- <form class="form-horizontal" action="/createdonor" method="POST"> -->
+        <!-- <form id="app" @submit="checkForm" action="https://vuejs.org/" method="post"> https://vuejs.org/v2/cookbook/form-validation.html -->
         <form class="form-horizontal">
+          <p v-if="errors.length"><b>Please correct the following error(s):</b>
+            <ul><li v-for="error in errors" :key="error">{{ error }}</li></ul>
+          </p>
           <div class="form-group">
             <label class="control-label col-sm-2">Full Name:</label>
             <input v-model="donor.name" type="text" width="50">
@@ -69,15 +73,62 @@ export default {
         state: '',
         zip: '',
         phone: ''
-      }
+      },
+      errors: []
     };
   },
   methods: {
-    createUser: function () {
+    createUser: function (e) {
       console.log('submit button clicked');
-      // send this somewhere databasey
-      // get id
-      // go to donor info page and reload with donor
+      // validate here
+      if (!this.validateForm()) {
+        e.preventDefault();
+      } else {
+        console.log('passed client side validation');
+        // send this somewhere databasey for server side validation and addition to database
+        // get id
+        // go to donor info page and reload with donor
+      }
+    },
+
+    validateForm: function () {
+      this.errors = [];
+      this.validateFullname();
+      this.validatePassword();
+      this.validateEmail();
+      this.validateDisplayname();
+      if (!this.errors.length) return true;
+    },
+
+    validateFullname: function () {
+      // TODO: what is too long?
+      if (this.donor.name.length === 0) {
+        this.errors.push('Need a Full Name');
+      }
+    },
+
+    validateDisplayname: function () {
+      // TODO: what is too long?
+      if (this.donor.displayname.length === 0) {
+        this.errors.push('Need a Display Name');
+      }
+    },
+
+    validatePassword: function () {
+      var minPassLength = 4;
+      if (this.donor.password.length < minPassLength) {
+        this.errors.push('Password needs to be at least ' + minPassLength + ' characters long');
+      }
+      if (this.donor.password !== this.donor.confirm) {
+        this.errors.push('Passwords do not match');
+      }
+    },
+
+    validateEmail: function () {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(this.donor.email)) {
+        this.errors.push('Email not valid');
+      }
     }
   }
 };
