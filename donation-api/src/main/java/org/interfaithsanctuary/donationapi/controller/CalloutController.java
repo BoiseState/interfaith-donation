32f26cbfa92a953e007a3c63b009fd228adab44d
Brendan Tierney <BrendanTierney@u.boisestate.edu>
2018-02-24 17:26:22 -0700

    ensured all controller routes were consistent

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/CalloutController.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/CalloutController.java
@@ -5,21 +5,18 @@ import org.interfaithsanctuary.donationapi.repository.CalloutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
-@RequestMapping(path="/api/callouts")
+@RequestMapping(path="/callouts")
 public class CalloutController {
     @Autowired
     private CalloutRepository calloutRepository;
 
-    @RequestMapping(path="/all", method = RequestMethod.GET)
-    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
     public Iterable<Callout> getAllCallouts() {
         return calloutRepository.findAll();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public Callout getCalloutById(@PathVariable("id") long id)  {
         return calloutRepository.findOne(id);
     }
