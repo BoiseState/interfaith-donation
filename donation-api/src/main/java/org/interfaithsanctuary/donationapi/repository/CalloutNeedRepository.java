3bfd6494a93e6a63524c6c420f75e45c5561b943
Brendan Tierney <BrendanTierney@u.boisestate.edu>
2018-03-11 12:12:04 -0600

    callout need model has been created

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/CalloutNeedRepository.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/CalloutNeedRepository.java
@@ -1,12 +1,11 @@
 package org.interfaithsanctuary.donationapi.repository;
 
-import org.interfaithsanctuary.donationapi.model.Gift;
+import org.interfaithsanctuary.donationapi.model.CalloutNeed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "gifts", path = "gifts")
-public interface GiftRepository extends CrudRepository<Gift, Long> {
-
+@RepositoryRestResource(collectionResourceRel = "calloutNeeds", path = "calloutNeeds")
+public interface CalloutNeedRepository extends CrudRepository<CalloutNeed, Long> {
 }
