c076670eec0ec159f1856b52f7ae45b71a9c7574
Tyler Nicholls <tylernicholls@u.boisestate.edu>
2018-03-16 13:43:37 -0600

    tried to change linter

--- front-end-admin/.eslintrc.js
+++ front-end-admin/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
@@ -16,16 +16,15 @@ module.exports = {
     'standard'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
+    'space-before-function-paren': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow semi-colons
-    'semi': [2, 'always']
+    semi: [2, 'always']
   }
-}
+};
