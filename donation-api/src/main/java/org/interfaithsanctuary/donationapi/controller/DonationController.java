e24dd6d9c34570dfce2228de9a8b4a577d721bb8
wils0651 <wils0651@hotmail.com>
2018-03-15 22:06:17 -0600

    added @CrossOrigin to DonationController

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/DonationController.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/DonationController.java
@@ -8,12 +8,9 @@ import org.interfaithsanctuary.donationapi.repository.DonationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(path="/donations")
 public class DonationController {
