d80ede1ccea797024ab9dff1c4913a19a1baf55f
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 14:24:28 -0600

    added logic scaffolding for order button

--- front-end-client/src/pages/home/home.ts
+++ front-end-client/src/pages/home/home.ts
@@ -18,5 +18,15 @@ export class HomePage {
   toggleOrdering() {
     console.log("changeOrder button has been clicked");
     this.order = !this.order;
+
+    if (this.order) {
+      // The most relavent card results should display at the top, descending order.
+      // This is default behavior.
+      // TODO: Provide the default order of cards. Note: they should be in this order
+      // by default, but their order will have to be modified again if they have been
+      // reversed at least once.
+    } else {
+      // TODO: Reverse order of cards, ascending order.
+    }
   }
 }
