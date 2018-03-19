fa36dc41ad2a2fc5180198d326024438f4bb62ba
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 14:19:08 -0600

    button's icon changes on click

--- front-end-client/src/pages/home/home.ts
+++ front-end-client/src/pages/home/home.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AccountPage } from '../account/account';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AccountPage } from "../account/account";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  order: boolean = true;
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(public navCtrl: NavController) {}
 
   pushAccountPage() {
     this.navCtrl.push(AccountPage);
   }
 
+  toggleOrdering() {
+    console.log("changeOrder button has been clicked");
+    this.order = !this.order;
+  }
 }
