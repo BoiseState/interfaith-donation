51aa2c0842af1e185d2b7eb28e4c13f090c82c50
wils0651 <wils0651@hotmail.com>
2018-03-18 19:14:15 -0600

    formatting register pages

--- front-end-admin/src/components/donor/RegisterDonor.vue
+++ front-end-admin/src/components/donor/RegisterDonor.vue
@@ -1,52 +1,54 @@
 <template>
   <div class="adduser">
     <div class="jumbotron">
-      <h2>Create New Donor</h2>
-      <form v-on:submit.prevent="onFormSubmit" class="form-horizontal">
-        <div class="form-group">
-          <label class="control-label col-sm-2">Full Name:</label>
-          <input v-model="donor.name" name="name" type="text" width="50" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">Email Address:</label>
-          <input v-model="donor.email" name="email" type="email" width="50" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">Phone:</label>
-          <input v-model="donor.phone" name="phone" type="text" width="50" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">Address:</label>
-          <input v-model="donor.address" name="address" type="text" width="100" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">City:</label>
-          <input v-model="donor.city" name="city" type="text" width="50" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">State:</label>
-          <input v-model="donor.state" name="state" type="text" width="2" required>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2">Postal Code:</label>
-          <input v-model="donor.zip" name="zip" type="text" width="10" required>
-        </div>
-        <!-- <div class="form-group">
-          <label class="control-label col-sm-2">Password:</label>
-          <input v-model="donor.password" name="password" type="text" width="10" required>
-        </div> -->
-        <button type="submit">Create Donor</button>
-      </form>
+      <div class="container">
+        <h2>Create New Donor</h2>
+        <form v-on:submit.prevent="onFormSubmit" class="form-horizontal">
+          <div class="form-group">
+            <label class="control-label col-sm-2">Full Name:</label>
+            <input v-model="donor.name" name="name" type="text" width="50" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">Email Address:</label>
+            <input v-model="donor.email" name="email" type="email" width="50" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">Phone:</label>
+            <input v-model="donor.phone" name="phone" type="text" width="50" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">Address:</label>
+            <input v-model="donor.address" name="address" type="text" width="100" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">City:</label>
+            <input v-model="donor.city" name="city" type="text" width="50" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">State:</label>
+            <input v-model="donor.state" name="state" type="text" width="2" required>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2">Postal Code:</label>
+            <input v-model="donor.zip" name="zip" type="text" width="10" required>
+          </div>
+          <!-- <div class="form-group">
+            <label class="control-label col-sm-2">Password:</label>
+            <input v-model="donor.password" name="password" type="text" width="10" required>
+          </div> -->
+          <button type="submit">Create Donor</button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {createDonor} from '../../services/donor-service';
+import { createDonor } from '../../services/donor-service';
 
 export default {
   name: 'register-donor',
-  data () {
+  data() {
     return {
       donor: {
         name: '',
@@ -63,7 +65,7 @@ export default {
     };
   },
   methods: {
-    onFormSubmit () {
+    onFormSubmit() {
       createDonor(this.donor);
     }
   }
