72fcabfa90a04da51bdaaa7baa799ce933a16064
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 22:13:55 -0600

    receiving html request data

--- front-end-client/src/pages/home/home.ts
+++ front-end-client/src/pages/home/home.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AccountPage } from '../account/account';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AccountPage } from "../account/account";
+import { CalloutService } from "../../services/calloutService"; // REMOVE THIS!
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(
+    public navCtrl: NavController,
+    public calloutService: CalloutService
+  ) {}
 
   pushAccountPage() {
     this.navCtrl.push(AccountPage);
+    this.calloutService.getCalloutNeedsMockJSONHttp(); // REMOVE THIS!
   }
-
 }
--- front-end-client/src/services/calloutService.ts
+++ front-end-client/src/services/calloutService.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import { HttpModule } from "@angular/http";
 import { File } from "@ionic-native/file";
 
 import "rxjs/add/operator/map";
 
 @Injectable()
 export class CalloutService {
-  constructor(private http: Http, private file: File) {}
+  constructor(
+    private http: Http,
+    private httpModule: HttpModule,
+    private file: File
+  ) {}
 
   getCalloutNeedsJSON() {
     let jsonObj;
@@ -32,6 +37,23 @@ export class CalloutService {
       )
     );
 
-    return jsonObj;
+    //return jsonObj;
+  }
+
+  getCalloutNeedsMockJSONHttp() {
+    console.log("getting callout?");
+    //let jsonObj;
+    //this.http.get('assets/mockData/mockCalloutNeeds.json').map(res => res.json()).subscribe(data => { jsonObj = data;});
+    //console.log(this.http.get('assets/mockData/mockCalloutNeeds.json'));
+
+    //console.log(this.http.get('assets/mockData/mockCalloutNeeds.json').map(data => data.json()));
+
+    console.log("response:");
+
+    this.http.get("assets/mockData/mockCalloutNeeds.json").subscribe(data => {
+      console.log(JSON.stringify(data));
+    });
+
+    //return jsonObj;
   }
 }
