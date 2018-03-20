6344e20058bba431376372cb0dc2cac8ab48b592
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 21:11:23 -0600

    added plugins to module

--- front-end-client/src/app/app.module.ts
+++ front-end-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
+import { HttpModule } from "@angular/http";
+import { File } from "@ionic-native/file";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
@@ -14,13 +16,14 @@ import { CalloutComponent } from "../components/callout/callout";
 
 @NgModule({
   declarations: [MyApp, HomePage, AccountPage, CalloutComponent],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), HttpModule],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage, AccountPage],
   providers: [
     StatusBar,
     SplashScreen,
     CalloutService,
+    File,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
