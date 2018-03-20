2611f6fb46eeb1824fe45510a2485641f3e26698
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 21:09:47 -0600

    added getters to get calloutNeeds JSON

--- front-end-client/src/services/calloutService.ts
+++ front-end-client/src/services/calloutService.ts
@@ -1,5 +1,37 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import { File } from "@ionic-native/file";
+
+import "rxjs/add/operator/map";
 
 @Injectable()
-export class CalloutService {}
+export class CalloutService {
+  constructor(private http: Http, private file: File) {}
+
+  getCalloutNeedsJSON() {
+    let jsonObj;
+
+    this.http
+      .get("donation-api/calloutneeds")
+      .map(res => res.json())
+      .subscribe(data => {
+        jsonObj = data;
+      });
+
+    return jsonObj;
+  }
+
+  // This is temporary until the donation-api call works and is reliable.
+  getCalloutNeedsMockJSON() {
+    let jsonObj;
+
+    console.log(
+      this.file.readAsText(
+        "../../www/assets/mockData/mockCalloutNeeds",
+        "data.json"
+      )
+    );
+
+    return jsonObj;
+  }
+}
