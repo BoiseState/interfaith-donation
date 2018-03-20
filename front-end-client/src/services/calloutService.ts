a9fb49b173c1d82f505b360f6819446826da3972
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-03-19 23:20:53 -0600

    cleaned code and added docs

--- front-end-client/src/services/calloutService.ts
+++ front-end-client/src/services/calloutService.ts
@@ -1,92 +1,39 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { HttpModule } from "@angular/http";
-import { File } from "@ionic-native/file";
-
-import "rxjs/add/operator/map";
 
 @Injectable()
 export class CalloutService {
-  constructor(
-    private http: Http,
-    private httpModule: HttpModule,
-    private file: File
-  ) {}
+  constructor(private http: Http, private httpModule: HttpModule) {}
 
-  getCalloutNeedsJSON() {
-    let jsonObj;
-
-    this.http
-      .get("donation-api/calloutneeds")
-      .map(res => res.json())
-      .subscribe(data => {
-        jsonObj = data;
-      });
-
+  /**
+   * Gets the calloutNeeds JSON from the donation-api endpoint.
+   */
+  async getCalloutNeedsJSON() {
+    let jsonObj = await this.doRequest("donation-api/calloutneeds");
     return jsonObj;
   }
 
-  // This is temporary until the donation-api call works and is reliable.
-  getCalloutNeedsMockJSON() {
-    let jsonObj;
-
-    console.log(
-      this.file.readAsText(
-        "../../www/assets/mockData/mockCalloutNeeds",
-        "data.json"
-      )
-    );
-
-    //return jsonObj;
-  }
-
-  async getCalloutNeedsMockJSONHttp() {
-    let jsonObj;
-    //this.http.get('assets/mockData/mockCalloutNeeds.json').map(res => res.json()).subscribe(data => { jsonObj = data;});
-    //console.log(this.http.get('assets/mockData/mockCalloutNeeds.json'));
-
-    //console.log(this.http.get('assets/mockData/mockCalloutNeeds.json').map(data => data.json()));
-
-    /* // WORKS!!!!
-    let promise = new Promise((resolve, reject) => {
-      let apiURL = 'assets/mockData/mockCalloutNeeds.json';
-      this.http.get(apiURL)
-        .toPromise()
-        .then(
-          res => {
-            //res.json() // Contains the correct json object
-            console.log("json response:");
-            console.log(res.json());
-            //jsonObj = JSON.parse(JSON.stringify(res.json()));
-            resolve();
-          }
-        )
-    });
-*/
-
-    console.log("response:");
-    jsonObj = await this.doRequest();
-
-    /* // works, gets the correct response but has issues with accessing body
-    this.http.get("assets/mockData/mockCalloutNeeds.json").subscribe(data => {
-      console.log(JSON.stringify(data));
-    });
-    */
-
-    console.log("jsonObj:");
-    console.log(jsonObj);
+  /**
+   * Gets the calloutNeeds JSON as local mock data.
+   */
+  async getCalloutNeedsMockJSON() {
+    let jsonObj = await this.doRequest("assets/mockData/mockCalloutNeeds.json");
     return jsonObj;
   }
 
-  private doRequest() {
+  /**
+   * Performs the request of getting JSON data from an restful API endpoint.
+   * @param apiUrl The api url endpoint the request from.
+   */
+  private doRequest(apiUrl: string) {
     return new Promise((resolve, reject) => {
-      let apiURL = "assets/mockData/mockCalloutNeeds.json";
+      //let apiURL = "assets/mockData/mockCalloutNeeds.json";
       this.http
-        .get(apiURL)
+        .get(apiUrl)
         .toPromise()
         .then(res => {
           res.json(); // Contains the correct json object
-          //jsonObj = JSON.parse(JSON.stringify(res.json()));
           resolve(res.json());
         });
     });
