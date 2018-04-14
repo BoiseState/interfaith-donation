5a8767085c69e929ba599f506bf02fc40aa62a63
Amy Feiling <amyfeiling@u.boisestate.edu>
2018-04-14 16:48:27 -0600

    added ability to get all donations by a donorId

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/DonationRepository.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/repository/DonationRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "donations", path = "donations")
 public interface DonationRepository extends PagingAndSortingRepository<Donation, Long> {
+
+    List<Donation> findByDonorId(long donorId);
 }
