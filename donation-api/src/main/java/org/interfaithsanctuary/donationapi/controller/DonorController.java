d9bfb05e902cbe7187fbcc596af6cc9f07b247c4
Luke Bosse <lukebosse@u.boisestate.edu>
2018-04-14 18:04:52 -0600

    added donor CRUD methods

--- donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/DonorController.java
+++ donation-api/src/main/java/org/interfaithsanctuary/donationapi/controller/DonorController.java
@@ -1,11 +1,16 @@
 package org.interfaithsanctuary.donationapi.controller;
 
+import java.net.URI;
+import java.util.Optional;
+
 import org.interfaithsanctuary.donationapi.model.Donor;
 import org.interfaithsanctuary.donationapi.repository.DonorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -21,9 +26,35 @@ public class DonorController {
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public Donor getDonorById(@PathVariable("id") long id)  {
         return donorRepository.findOne(id);
+    }
+    
+    @CrossOrigin
+    @PostMapping(value = "/")
+    public ResponseEntity<Donor> createDonor(@RequestBody Donor donor) {
+        Donor savedDonor = donorRepository.save(donor);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").
+                buildAndExpand(savedDonor.getId()).toUri();
+        return ResponseEntity.created(location).body(savedDonor);
+    }
+    
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public void deleteDonorById(@PathVariable("id") long id) {
+        donorRepository.delete(id);
+    }
+
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity<Donor> updateDonorById(@RequestBody Donor donor, @PathVariable("id") long id) {
+        Optional<Donor> donorOptional = Optional.ofNullable(donorRepository.findOne(id));
+        if(!donorOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        donorRepository.save(donor);
+        return ResponseEntity.ok().build();
     }
 }
 
