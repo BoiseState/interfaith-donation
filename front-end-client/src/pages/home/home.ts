e7011ddf20a72dc3cf2ad5033305f7725ced35de
Greg Brasier <gregorybrasier@u.boisestate.edu>
2018-04-18 11:55:07 -0600

    implemented toggle-ordering button

--- front-end-client/src/pages/home/home.ts
+++ front-end-client/src/pages/home/home.ts
@@ -8,7 +8,6 @@ import { CalloutService } from "../../services/calloutService";
   templateUrl: "home.html"
 })
 export class HomePage {
-  order: boolean = true;
   private calloutNeeds: any[];
   private callouts: any[];
   private needs: NeedObj[];
@@ -54,18 +53,7 @@ export class HomePage {
   }
 
   toggleOrdering() {
-    console.log("changeOrder button has been clicked");
-    this.order = !this.order;
-
-    if (this.order) {
-      // The most relavent card results should display at the top, descending order.
-      // This is default behavior.
-      // TODO: Provide the default order of cards. Note: they should be in this order
-      // by default, but their order will have to be modified again if they have been
-      // reversed at least once.
-    } else {
-      // TODO: Reverse order of cards, ascending order.
-    }
+    this.needs.reverse();
   }
 }
 
